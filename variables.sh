3ded6f30d765a051ee6550d60f1331e145bd4ea0
laharika4 <sandrakala4444@gmail.com>
2025-09-01 16:03:24 +0530

    new change

--- variables.sh
+++ variables.sh
@@ -4,6 +4,7 @@
  echo  # prints cursor goes to next line
  read -s -p "please enter passowrd :: " PASSWORD
  echo # prints in nextline
+ echo # for one line space 
  echo "username is $USERNAME"
  echo #prints in next line
- echo "password is$PASSWORD"
+ echo "password is $PASSWORD"
