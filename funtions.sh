0a9a9546520e456dfd43e591a304dec826b8ad32
laharika4 <sandrakala4444@gmail.com>
2025-09-02 12:45:55 +0530

    mysql installation

---
+++ funtions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+userid=$(id -u)
+VALIDATE (){
+if [ $1 -ne 0]
+ then 
+   echo "$2 ...Failed"
+else
+    echo "$2 ...Success"
+fi
+
+}
+
+if [ $userid -ne 0 ]
+ then 
+    echo "you donn't have permixxion to this installation"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+ then 
+    dnf install mysql -y
+      VALIDATE $? "installation of mysql"
+else
+    echo "mysql already installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+ then
+    dnf install git -y
+     VALIDATE $? "installation of git"
+else
+  echo "git already installed"
+fi
