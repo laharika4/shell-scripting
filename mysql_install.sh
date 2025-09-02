3a76938efc9227c08b8f3cd7354522b0283d4a74
laharika4 <sandrakala4444@gmail.com>
2025-09-02 11:51:37 +0530

    mysql installation

---
+++ mysql_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+ then 
+    echo "you donn't have permixxion to this installation"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+ then 
+    dnf install mysql -y
+      if [ $? -ne 0 ]
+       then 
+          echo "mysql not installed ..Failure"
+          exit 
+        else
+           echo "mysql installation ... Successful"
+        fi
+else
+    echo "mysql already installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+ then
+    dnf install git -y
+     if [ $? -ne 0 ]
+      then 
+         echo "git installation failed"
+         exit 1
+    else
+      echo "git installed successfully"
+    fi
+else
+  echo "git already installed"
