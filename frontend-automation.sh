2e019299951ef58f3c1bf3b7a57887f1c488c4cf
laharika4 <sandrakala4444@gmail.com>
2025-09-03 21:27:34 +0530

    new change

--- frontend-automation.sh
+++ frontend-automation.sh
@@ -8,12 +8,12 @@ y='\e[33m'
 n='\e[0m'
 
 log_path="/var/log/frontend_logs"
-file=$(echo $0 | cut -d "." -f)
+file=$(echo $0 | cut -d "." -f1)
 timestampe=$(date +%Y-%m-%d-%H-%M-%s)
 log_filename="$log_path/$file-$timestampe"
 
 variable(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
      then 
         echo -e "$2 .....$r Failure $n"
         exit 1
@@ -54,7 +54,7 @@ variable $? "changing directory to html"
 unzip /tmp/frontend.zip
 variable $? "extracting code"
 
-cp /home/ec2-user/git/shell-scripting/frontend.conf  /etc/nginx/defaul.d/frontend.conf
+cp /home/ec2-user/git/shell-scripting/frontend.conf  /etc/nginx/default.d/frontend.conf
 variable $? "copying configuration file"
 
 systemctl restart nginx
