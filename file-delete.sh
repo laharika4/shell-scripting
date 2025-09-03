eea556318a866f654d97a97152943c26801c3e3f
laharika4 <sandrakala4444@gmail.com>
2025-09-03 22:16:23 +0530

    new change

---
+++ file-delete.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+userid=(id -u)
+
+r="\e[31m"
+g="\e[32m"
+n="\e[0m"
+
+sourcefolder="/var/log/frontend_logs"
+
+if [ $userid -ne 0 ]
+  then 
+    echo -e "$r no permission for this user $n"
+    exit 1
+fi
+
+files_tobe_deleted=(find . $sourcefolder -name "*.log" -mtime +14)
+echo "files that are to be deleted: $files_tobe_deleted"
+
+while read -r file
+do 
+   echo "listing all files to b deleted: $file"
+   rm -rf $file
+
+done <<< $files_tobe_deleted
